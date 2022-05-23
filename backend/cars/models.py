63d91f85692ea3531338461bf114657aaff1e36b
Rafael Diaz <crudesailorphotographysd@gmail.com>
2022-05-23 07:52:12 -0700

    Class for contracts created

--- backend/cars/models.py
+++ backend/cars/models.py
@@ -2,7 +2,12 @@ from django.db import models
 from authentication.models import User
 
 # Create your models here.
-class Contracts(model.mMdels)
+class Contracts(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    details = models.CharField(max_length=200)
+    company_details = models.CharField(max_length=100)
+    year_contract_started = models.IntegerField()
+
 # <<<<<<<<<<<<<<<<< EXAMPLE FOR STARTER CODE USE <<<<<<<<<<<<<<<<<
 
 
